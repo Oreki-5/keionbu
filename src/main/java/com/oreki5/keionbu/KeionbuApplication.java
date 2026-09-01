a38701844936f88b642af4f48261ca30e3962e96
Oreki-5 <adityacoc1602@gmail.com>
2026-09-01 10:36:21 +0530

    setup base project

---
+++ src/main/java/com/oreki5/keionbu/KeionbuApplication.java
@@ -0,0 +1,13 @@
+package com.oreki5.keionbu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeionbuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeionbuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oreki5/keionbu/KeionbuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oreki5.keionbu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeionbuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
