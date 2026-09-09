f217d76a5acb8f5340185352bb8120af6a363ffc
Oreki-5 <adityacoc1602@gmail.com>
2026-09-09 17:38:56 +0530

    started with security config

--- src/main/java/com/oreki5/keionbu/dtoModels/students/StudentsCreateRes.java
+++ src/main/java/com/oreki5/keionbu/dtoModels/students/StudentsCreateRes.java
@@ -12,11 +12,11 @@ public class StudentsCreateRes implements UserAccountResponse, StudentsResponse 
     @NotEmpty
     private String id;
     @NotEmpty
-    private String username;
-    @NotEmpty
     private String studentName;
     @NotEmpty
     private String email;
+    @NotEmpty
+    private String username;
     @NotEmpty
     private boolean verified;
 
